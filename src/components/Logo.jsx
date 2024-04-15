fc3a5ba327f776b6c8367084a97d4f6cc1e4964b
FarheenMalak <115576023+FarheenMalak@users.noreply.github.com>
2024-04-15 11:21:48 -0700

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Logo = ({width = '100px'}) => {
   return (
-    <div className=''style={{ width: '200px' }}><img src="src/mylogo.png" alt="logo" /></div>
+    <div className=''style={{ width: '200px' }}><img src="/mylogo.png" alt="" /></div>
   )
 }
 
