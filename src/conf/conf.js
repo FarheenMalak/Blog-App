dd4860fa26313177951005a2e251733cc2490720
FarheenMalak <115576023+FarheenMalak@users.noreply.github.com>
2024-04-15 12:31:48 -0700

    Update conf.js

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,11 +1,10 @@
 const conf = {
     appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteProjectId :  String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASEID),
     appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-
-}
-
-
+    openAiKey : String(import.meta.env.VITE_OPENAI_API_KEY ),
+    tinyMCEApiKey : String(import.meta.env.VITE_TINYMCE_API_KEY)
+    }
 export default conf
